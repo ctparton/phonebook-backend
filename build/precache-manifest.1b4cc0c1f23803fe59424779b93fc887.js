416097dcd6f2aacff3c08b29abc84cf906c51cc8
Callum Desktop <ctpartondev@gmail.com>
2020-07-29 00:46:15 +0100

    added frontend build, changed backend to serve this

---
+++ build/precache-manifest.1b4cc0c1f23803fe59424779b93fc887.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99e98599749c81964dab0bb67550e2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a6bfc8cdd5513f62307",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dbaf8825220e57e56d18",
+    "url": "/static/js/2.ad435a02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad435a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a6bfc8cdd5513f62307",
+    "url": "/static/js/main.065b9121.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const { response } = require('express')
 
 const app = express()
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 
